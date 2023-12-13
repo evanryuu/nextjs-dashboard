a2970af3981338afd1c758055eae45942484ae86
evanryuu <evanryuu@outlook.com>
2023-12-13 23:23:33 +0800

    chore: finish ch15 - Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,10 +4,12 @@
 // making them extremely versatile.
 'use server';
 
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import { AuthError } from 'next-auth'
 
 const FormSchema = z.object({
   id: z.string(),
@@ -25,6 +27,25 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 // This is temporary until @types/react-dom is updated
 export type State = {
